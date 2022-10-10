c11d6d81998d7c54351c6c0bdde34cb354c6959c
Chloë <iris65del@gmail.com>
2022-10-10 15:09:42 +0200

    modif test

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -21,11 +21,11 @@ public class Test {
 		Console c2 = new Console("Nintendo Wii",29.99,LocalDate.parse("2022-01-01"));
 		Console c3 = new Console("Nintendo DS",29.99,LocalDate.parse("2022-01-01"));
 
-		Jeu j1 =new Jeu("Mario Kart",c1);
-		Jeu j2 =new Jeu("Let's go Pikachu",c1);
-		Jeu j3 =new Jeu("Metroid Dread",c1);
-		Jeu j4 =new Jeu("Zelda",c2);
-		Jeu j5 =new Jeu("Super Mario Bross",c3);
+		Jeu j1 =new Jeu("Mario Kart",c1,b1);
+		Jeu j2 =new Jeu("Let's go Pikachu",c1,b1);
+		Jeu j3 =new Jeu("Metroid Dread",c1,b1);
+		Jeu j4 =new Jeu("Zelda",c2,b1);
+		Jeu j5 =new Jeu("Super Mario Bross",c3,b1);
 		
 		Achat ac1 = new Achat(j2, LocalDate.parse("2022-10-01"), 49.99);
 		Achat ac2 = new Achat(j4, LocalDate.parse("2022-09-25"), 51.99);
