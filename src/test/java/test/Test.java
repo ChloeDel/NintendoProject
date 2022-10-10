69caf9419e0b3aceb5d50d55f5dfde3ba9dfc2b6
Chloë <iris65del@gmail.com>
2022-10-10 15:26:31 +0200

    Create new hybride

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -8,6 +8,7 @@ import metier.Portable;
 import metier.Salon;
 import metier.Achat;
 import metier.Client;
+import metier.Hybride;
 import metier.Adresse;
 import metier.Boutique;
 
@@ -21,13 +22,13 @@ public class Test {
 
 		Portable P = new Portable("Nintendo Switch",29.99,LocalDate.parse("2022-01-01"));
 		Salon S = new Salon ("Nintendo Wii",29.99,LocalDate.parse("2022-01-01"));
-		Portable c3 = new Portable("Nintendo DS",29.99,LocalDate.parse("2022-01-01"));
+		Hybride H = new Hybride("Nintendo DS",29.99,LocalDate.parse("2022-01-01"));
 
 		Jeu j1 =new Jeu("Mario Kart",P,b1);
 		Jeu j2 =new Jeu("Let's go Pikachu",P,b1);
 		Jeu j3 =new Jeu("Metroid Dread",P,b1);
 		Jeu j4 =new Jeu("Zelda",S,b1);
-		Jeu j5 =new Jeu("Super Mario Bross",c3,b1);
+		Jeu j5 =new Jeu("Super Mario Bross",H,b1);
 		
 		Achat ac1 = new Achat(j2, LocalDate.parse("2022-10-01"), 49.99);
 		Achat ac2 = new Achat(j4, LocalDate.parse("2022-09-25"), 51.99);
