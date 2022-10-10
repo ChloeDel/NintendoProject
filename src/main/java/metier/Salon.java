7a9306ffcd4da3eed6d777e5f20d50fd6392e31c
Oumaima AZAMI <oumaima_azami@hotmail.com>
2022-10-10 14:05:20 +0100

    Ajout Salon

---
+++ src/main/java/metier/Salon.java
@@ -0,0 +1,12 @@
+package metier;
+
+import java.time.LocalDate;
+
+public class Salon extends Console {
+
+	public Salon(String nom, double prix, LocalDate dateDeSortie) {
+		super(nom, prix, dateDeSortie);
+		
+	}
+
+}
