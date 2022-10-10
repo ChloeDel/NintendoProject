d436a938085ff2e2ba4314216bceb7e6ef697a61
Samuel Delabre <delabresamuel@gmail.com>
2022-10-10 14:36:18 +0200

    add new adress a1 and boutique b1 in main

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -1,9 +1,15 @@
 package test;
 
+import metier.Adresse;
+import metier.Boutique;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Adresse a1 = new Adresse (6,"Rue Mario", "Royaume champignon");
+		Boutique b1 = new Boutique("Le paradis du jeu vidéo",a1);
 
 	}
 
